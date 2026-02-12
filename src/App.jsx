ef8c762f74ebcba3eab33a8c64a05b850de090f8
razvan balan <2473254@cognizant.com>
2026-02-12 11:31:53 +0200

    fixed select issue for watchlist

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,15 @@ import "./App.css";
 import { use, useState } from "react";
 import { useLocalStorage } from "./storage/useLocalStorage";
 
-
 export default function App() {
   const [search, setSearch] = useState("");
   const [genre, setGenre] = useState("");
   const [rating, setRating] = useState("");
   const [watchlist, setWatchlist] = useLocalStorage("movieWatchList", []);
-  const [showWatchList, setShowWatchList] = useState(false); 
-
+  const [showWatchList, setShowWatchList] = useState(false);
 
   const addToWatchlist = (movie) => {
-    if(!watchlist.find((m) => m.id === movie.id)) {
+    if (!watchlist.find((m) => m.id === movie.id)) {
       setWatchlist([...watchlist, movie]);
     }
   };
@@ -28,60 +26,58 @@ export default function App() {
     return watchlist.some((movie) => movie.id === movieId);
   };
 
+  const applyFilters = (movielist) => {
+    return movielist.filter((movie) => {
+      const movieTitle = movie.title.toLowerCase();
+      const searchLower = search.toLowerCase();
+      const matchesSearch = movieTitle.includes(searchLower);
 
-  const filteredMovies = movies.filter((movie) => {
-    
-    const movieTitle = movie.title.toLowerCase();
-    const searchLower = search.toLowerCase();
-    const matchesSearch = movieTitle.includes(searchLower);
+      let matchesGenre = false;
+      if (genre === "") {
+        matchesGenre = true;
+      } else {
+        matchesGenre = movie.genre === genre;
+      }
 
-    let matchesGenre = false;
-    if(genre === "") {
-      matchesGenre = true;
-    } else {
-      matchesGenre = movie.genre === genre;
-    }
+      let matchesRating = false;
+      if (rating === "") {
+        matchesRating = true;
+      } else {
+        const movieRating = parseFloat(movie.rating);
+        const minRating = parseFloat(rating);
+        matchesRating = movieRating >= minRating;
+      }
 
-    let matchesRating = false;
-    if(rating === "") {
-      matchesRating = true;
-    } else {
-      const movieRating = parseFloat(movie.rating);
-      const minRating = parseFloat(rating);
-      matchesRating = movieRating >= minRating;
-    }
+      if (matchesSearch && matchesRating && matchesGenre) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  };
 
-    if(matchesSearch && matchesRating && matchesGenre) {
-      return true;
-    } else {
-      return false;
-    }
-  
-  });
-
-
-  const moviesToDisplay = showWatchList ? watchlist : filteredMovies;
+  const moviesToDisplay = showWatchList ? applyFilters(watchlist) : applyFilters(movies);
 
   return (
     <>
-      <Header 
-        search={search} 
-        onSearchChange={setSearch} 
-        genre = {genre}
-        onGenreChange = {setGenre}
-        rating = {rating}
-        onRatingChange = {setRating}
-        showWatchList = {showWatchList}
-        onToggleWatchlist = {() => setShowWatchList(!showWatchList)}
-        watchlistCount = {watchlist.length}
-        />
+      <Header
+        search={search}
+        onSearchChange={setSearch}
+        genre={genre}
+        onGenreChange={setGenre}
+        rating={rating}
+        onRatingChange={setRating}
+        showWatchList={showWatchList}
+        onToggleWatchlist={() => setShowWatchList(!showWatchList)}
+        watchlistCount={watchlist.length}
+      />
       <main>
-        <MovieList 
-        movies={moviesToDisplay}
-        onAddToWatchlist = {addToWatchlist}
-        onRemoveFromWatchlist = {removeFromWatchlist}
-        isInWatchlist = {isInWatchlist}
-        showWatchList = {showWatchList}
+        <MovieList
+          movies={moviesToDisplay}
+          onAddToWatchlist={addToWatchlist}
+          onRemoveFromWatchlist={removeFromWatchlist}
+          isInWatchlist={isInWatchlist}
+          showWatchList={showWatchList}
         />
       </main>
     </>

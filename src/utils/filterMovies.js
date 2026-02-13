987cc431f55b5c80b66129deed02b454a77837a4
razvan balan <2473254@cognizant.com>
2026-02-13 13:16:21 +0200

    added fetching from online movies api

--- src/utils/filterMovies.js
+++ src/utils/filterMovies.js
@@ -2,9 +2,10 @@ export function filterMovies(movies, {search, genre, rating}) {
     return movies.filter((movie) => {
         const matchesSearch = !search || movie.title.toLowerCase().includes(search.toLowerCase());
 
-        const matchesGenre = !genre || movie.genre === genre;
+        const matchesGenre = !genre || (movie.genre && movie.genre.toLowerCase().includes(genre.toLowerCase()));
 
-        const matchesRating = !rating || parseFloat(movie.rating) >= parseFloat(rating);
+        const movieRating = parseFloat(movie.rating);
+        const matchesRating = !rating || (!isNaN(movieRating) && movieRating >= parseFloat(rating));
 
         return matchesSearch && matchesGenre && matchesRating;
 
